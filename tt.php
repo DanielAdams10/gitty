606e7f21f2c156fce4334e84a46459b31044d353
DanielAD10 <adamsdaniel171@gmail.com>
2022-05-11 07:58:34 +0630

    some code in tt.php

---
+++ tt.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <h1>Welcome to Foodie Paradise</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero sapiente, modi excepturi ea asperiores perspiciatis? Nostrum eos dolores commodi expedita consequatur dolore quia a quis, ut repellendus distinctio quidem sapiente alias mollitia harum voluptatum unde possimus illo placeat. Dolorum illum iste hic nisi molestiae. Corporis rerum eaque ipsum ad voluptates.</p>
+</body>
+</html>
